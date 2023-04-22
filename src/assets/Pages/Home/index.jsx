a3254fecd8f93326cf81f4efa60bc7624ee0b989
crystian97 <icrystianveloso@gmail.com>
2023-04-22 11:13:54 -0300

    feat:finished links

--- src/assets/Pages/Home/index.jsx
+++ src/assets/Pages/Home/index.jsx
@@ -11,10 +11,16 @@ export default function Home() {
           />
         </div>
         <div className={styles.SocialLinks}>
-          <a href="" target="_blank" rel="noopener noreferrer">
+          <a
+            className={styles.SocialLink}
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Linkedin
           </a>
           <a
+            className={styles.SocialLink}
             href="https://github.com/crystian97?tab=repositories"
             target="_blank"
             rel="noopener noreferrer"
@@ -22,7 +28,7 @@ export default function Home() {
             Github
           </a>
           <a
-            className={styles.whatsapp}
+            className={(styles.whatsapp, styles.SocialLink)}
             href="https://api.whatsapp.com/send?phone=31998414257&text=Ol%C3%A1%20Crystian%20Tudo%20bem?"
             target="_blank"
             rel="noopener noreferrer"
