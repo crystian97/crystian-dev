ad4c26da4dd6eb27b751617f9c5fbb3580c499d5
crystian97 <icrystianveloso@gmail.com>
2023-04-27 12:09:24 -0300

    refactor:code add recent projects

--- src/assets/Pages/Home/index.jsx
+++ src/assets/Pages/Home/index.jsx
@@ -1,9 +1,18 @@
-import { GithubLogo, LinkedinLogo, WhatsappLogo } from "phosphor-react";
+import {
+  Browser,
+  FileCss,
+  FileHtml,
+  FileJs,
+  FileJsx,
+  GithubLogo,
+  LinkedinLogo,
+  WhatsappLogo,
+} from "phosphor-react";
 import styles from "./Styles.module.css";
 export default function Home() {
   return (
-    <>
-      <div className={styles.HomeContainer}>
+    <div className={styles.HomeContainer}>
+      <div className={styles.HomeProfile}>
         <div className={styles.ProfilePhotoContainer}>
           <img
             className={styles.ProfileImage}
@@ -11,6 +20,7 @@ export default function Home() {
             alt="Crystian97"
           />
         </div>
+        <small className={styles.title}>crystian de araújo veloso</small>
         <div className={styles.SocialLinks}>
           <a
             className={styles.SocialLink}
@@ -37,8 +47,92 @@ export default function Home() {
             <WhatsappLogo size={32} />
           </a>
         </div>
-        <div>Projetos em breve...</div>
       </div>
-    </>
+      <div className={styles.ProjectsContainer}>
+        <h2>Projetos Recentes</h2>
+        <div className={styles.ProjectsCardContainer}>
+          <div className={styles.card}>
+            <div className={styles.cardBorderTop}></div>
+            <div className={styles.img}>
+              <img
+                src="https://github.com/crystian97/neflix-clone/raw/master/assets/cover_netflix_clone.png"
+                alt=""
+                className={styles.cardImage}
+              />
+            </div>
+            <span> Netflix Clone</span>
+            <div className={styles.job}>
+              {" "}
+              <FileJs size={32} />
+              <FileHtml size={32} />
+              <FileCss size={32} />
+            </div>
+            <div className={styles.cardFooter}>
+              <a href="https://neflix-clone-three.vercel.app/">
+                {" "}
+                <Browser size={16} /> Acesar
+              </a>
+              <a href="https://github.com/crystian97/neflix-clone">
+                {" "}
+                <GithubLogo size={16} /> Repositório
+              </a>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.cardBorderTop}></div>
+            <div className={styles.img}>
+              <img
+                src="https://github.com/crystian97/weather-forecast/raw/master/weather.png"
+                alt=""
+                className={styles.cardImage}
+              />
+            </div>
+            <span> Weather Forecast</span>
+            <div className={styles.job}>
+              {" "}
+              <FileJs size={32} /> <FileJsx size={32} />
+              <FileHtml size={32} />
+              <FileCss size={32} />
+            </div>
+            <div className={styles.cardFooter}>
+              <a href="https://weather-forecast-tau.vercel.app/">
+                {" "}
+                <Browser size={16} /> Acesar
+              </a>
+              <a href="https://github.com/crystian97/weather-forecast">
+                {" "}
+                <GithubLogo size={16} /> Repositório
+              </a>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.cardBorderTop}></div>
+            <div className={styles.img}>
+              <img
+                src="https://github.com/crystian97/todoApp/raw/master/todoApp.gif"
+                alt=""
+                className={styles.cardImage}
+              />
+            </div>
+            <span> todoApp</span>
+            <div className={styles.job}>
+              <FileJs size={32} /> <FileJsx size={32} />
+              <FileHtml size={32} />
+              <FileCss size={32} />
+            </div>
+            <div className={styles.cardFooter}>
+              <a href="https://todo-app-lac-kappa.vercel.app/">
+                {" "}
+                <Browser size={16} /> Acesar
+              </a>
+              <a href="https://github.com/crystian97/todoApp">
+                {" "}
+                <GithubLogo size={16} /> Repositório
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
